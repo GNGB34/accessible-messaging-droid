f3635e1e2a1614b4ae3ebd30e68f04ec84c9bfd7
Michael <mike.kagnew@gmail.com>
2020-11-10 00:38:45 -0500

    Client can stop FUNCTIONALITY now; service cannot be manually stopped

--- app/src/main/java/com/example/accessiblemessaging/MainActivity.java
+++ app/src/main/java/com/example/accessiblemessaging/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.accessiblemessaging;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,14 +19,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button button = (Button) findViewById(R.id.button);
+        Button stop = (Button) findViewById(R.id.stop);
+        Button check = (Button) findViewById(R.id.check); //Check if service is running or not
+
+        check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isMyServiceRunning(NotificationService.class);
+            }
+        });
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent service = new Intent(getApplicationContext(), NotificationService.class);
+                service.putExtra("START", true);
+                startService(service);
+            }
+        });
 
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent service = new Intent(getApplicationContext(), NotificationService.class);
+                service.putExtra("START", false);
                 startService(service);
             }
         });
     }
+
+    //Checks if the service is running; it basically always is unless the OS stops it
+    private void isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                Log.d("RUN", "Running");
+            } else Log.d("RUN", "not Running");
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/accessiblemessaging/NotificationService.java
+++ app/src/main/java/com/example/accessiblemessaging/NotificationService.java
@@ -1,59 +1,98 @@
 package com.example.accessiblemessaging;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.display.DisplayManager;
+import android.os.Build;
+import android.os.PowerManager;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
+
 public class NotificationService extends NotificationListenerService  {
 
-
-//    public void startNotificationListener() {
-//        //start's a new thread
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                //fetching notifications from server
-//                //if there is notifications then call this method
-//            }
-//        }).start();
-//    }
-
-
-
+    static boolean runFlag = false; //Use this to manipulate functions of the service since this service cannot be stopped manually; only the OS can do that
+    //Also used in case service gets disconnected by OS without his approval, request a rebind in the onServiceDisconnected()
 
     @Override
-    public void onCreate()
-    {
+    public void onCreate() {
         super.onCreate(); //Just default oncreate
     }
 
+    //ALlow app to stay open, keep the service running to if randomly stopped by OS
+    // Client can stop FUNCTIONALITY but not the service. This function can act as start AND stop for functionality
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d("STARTT","we start");
         //return super.onStartCommand(intent, flags, startId);
-        return START_STICKY;  //Keep the service running;
+
+        //Check if explicitly want things to stop, if not, will return previous intent (which is active)
+        boolean flag = intent.getBooleanExtra("START", true);
+        if (flag == true){
+            Log.d("STARTT","we start");
+            runFlag = true;
+            return START_REDELIVER_INTENT;  //Keep the service running;
+
+         //Means the functionality (i.e read back to him) should stop cause EXPLICITLY stated by client
+        } else {
+            Log.d("STOP","we stop");
+            runFlag = false;
+            //onDestroy();
+            return START_NOT_STICKY;
+        }
     }
 
+
+
+    //TODO Not sure why this code does not state connected
     @Override
     public void onListenerConnected (){
         Log.d("CONNECTED", "We connected boyz");
-        Toast.makeText(getApplicationContext(),"it works", Toast.LENGTH_LONG);
-
+        super.onListenerConnected();
     }
+
+    //Should only disconnect if the service is explicitly stopped; if it's not, it will attempt to rebind
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void onListenerDisconnected(){
-        Log.d("DISCONNECTED", "We DISCONNECTED boyz");
-
+        if(runFlag!=false){
+            super.onListenerDisconnected();
+            Log.d("DISCONNECTED", "We DISCONNECTED boyz");
+            Log.d("NLService", "Notification listener DISCONNECTED from the notification service! Scheduling a reconnect...");
+            requestRebind(new ComponentName(this.getPackageName(), this.getClass().getCanonicalName()));
+        }
     }
 
-    public void onNotificationPosted (StatusBarNotification sbn){
 
-        String title = sbn.getNotification().extras.getString("android.title");
-        String text = sbn.getNotification().extras.getString("android.text");
-        String package_name = sbn.getPackageName();
-        Log.d("Notification", title);
-        Log.d("Notification:", text);
+
+    //When a notification is posted, if the app is set to run, can do needed operations
+    public void onNotificationPosted (StatusBarNotification sbn){
+        //TODO PUT HERE OTHER FUNCTIONS IN IF STATEMENT SUCH AS CLOUD TRANSLATION
+        if (runFlag == true && checkScreen()){
+            String title = sbn.getNotification().extras.getString("android.title");
+            String text = sbn.getNotification().extras.getString("android.text");
+            String package_name = sbn.getPackageName();
+            Log.d("Notification", title);
+            Log.d("Notification:", text);
+        }
+    }
+
+//    @Override
+//    public void onDestroy(){
+//        runFlag = false;
+//        Log.d("STOP", "stopp");
+//        super.onDestroy();
+//
+//    }
+
+    //Screen has to allow for functionality; don't do things while screen is off
+    private boolean checkScreen(){
+        PowerManager powerManager = (PowerManager) getSystemService(POWER_SERVICE);
+        boolean result= Build.VERSION.SDK_INT>= Build.VERSION_CODES.KITKAT_WATCH&&powerManager.isInteractive()|| Build.VERSION.SDK_INT< Build.VERSION_CODES.KITKAT_WATCH&&powerManager.isScreenOn();
+        return result;
+
     }
 
 }
