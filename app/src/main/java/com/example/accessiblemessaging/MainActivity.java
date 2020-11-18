765d144cd960bae8d8614e81094bdb56b63708f5
Michael <mike.kagnew@gmail.com>
2020-11-18 16:14:52 -0500

    some very experimental google assistant changes; off of mike branch

--- app/src/main/java/com/example/accessiblemessaging/MainActivity.java
+++ app/src/main/java/com/example/accessiblemessaging/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.accessiblemessaging;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,8 +11,11 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    public static NaturalLanguageService.OUTPUT_STATES state;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,21 +26,18 @@ public class MainActivity extends AppCompatActivity {
         Button settings=(Button) findViewById(R.id.settings);
 
 
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent service = new Intent(getApplicationContext(), NotificationService.class);
-                service.putExtra("START", NaturalLanguageService.OUTPUT_STATES.VOICE);
-                startService(service);
+                startListen();
             }
         });
 
         stop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent service = new Intent(getApplicationContext(), NotificationService.class);
-                service.putExtra("START", NaturalLanguageService.OUTPUT_STATES.STOP);
-                startService(service);
+                stopListen();
             }
         });
 
@@ -47,6 +48,9 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        handleIntent();
+
     }
 
     //Checks if the service is running; it basically always is unless the OS stops it
@@ -66,4 +70,44 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void stopListen(){
+        Intent service = new Intent(getApplicationContext(), NotificationService.class);
+        service.putExtra("START", NaturalLanguageService.OUTPUT_STATES.STOP);
+        startService(service);  //Using the startService, but passing the STOP state to stop functionality
+    }
+
+    public void startListen(){
+        Intent service = new Intent(getApplicationContext(), NotificationService.class);
+        service.putExtra("START", NaturalLanguageService.OUTPUT_STATES.VOICE);
+        startService(service);
+    }
+
+    private void handleIntent(){
+        //if the intent matches the ACTION_VIEW (meaning Google requested) then enter it)
+        Intent intent = getIntent();
+
+        if (intent.getAction() == intent.ACTION_VIEW) {
+           handleDeepLink(intent.getData());
+            Log.d("ACTION", "made it to action");
+            Log.d("URI", intent.getData().toString());
+
+        }
+    }
+
+    private void handleDeepLink(Uri data){
+
+        List<String> arr = data.getPathSegments();
+        if (arr.contains("start")){
+            Log.d("START_URI", "Able to make it to deep link start");
+            startListen();
+        } else if (arr.contains("stop")){
+            stopListen();
+        }else{
+            Log.d("NOTHING_ELSE", "the else ");
+            for (String s: arr){
+                Log.d("PATHS_URI", s);
+            }
+        }
+
+    }
 }
